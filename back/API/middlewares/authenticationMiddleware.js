7fbbc3af88e2e74c266034c2d4cd353a1fa726b3
Fab <fabou291@gmail.com>
2021-06-21 22:31:25 +0200

    implemented authentication middleware v2

--- back/API/middlewares/authenticationMiddleware.js
+++ back/API/middlewares/authenticationMiddleware.js
@@ -5,59 +5,54 @@ import authenticationHandler from "../helpers/authenticationHandler.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-const parseRefreshToken = (req, res) => {
-    console.log('yo2')
-    const error = createHttpError.Unauthorized("Invalid Token");
-
-    if (!req.cookies || !req.cookies.refreshToken) return error;
-
+const isValidToken = (req, tokenParams) => {
     try {
-        const decodedToken = JWT.verify(
-            req.cookies.refreshToken,
-            process.env.SECRET_REFRESH_TOKEN
-        );
+        if (!req.headers.authorization) return false;
+
+        const decodedToken = JWT.verify(...tokenParams);
+
+        if (
+            !decodedToken ||
+            (req.body.userId && req.body.userId !== decodedToken.userId)
+        )
+            return false;
     } catch (err) {
-        return error;
+        if (err && err.name == "TokenExpiredError") return "expired";
+        else return false;
     }
 
-    if (req.body.userId && req.body.userId !== decodedToken.userId)
-        return error;
-
-    const token = authenticationHandler.createToken();
-    const refreshToken = authenticationHandler.createRefreshToken();
-    res.cookie("refreshToken", refreshToken);
-
-    return;
+    return true;
 };
 
-const parseToken = (req, res) => {
-    const error = createHttpError.Unauthorized("Invalid Token");
-
-    if (!req.headers.authorization) return error;
-
-    try {
-        const decodedToken = JWT.verify(
-            req.headers.authorization.split(" ")[1],
-            process.env.SECRET_TOKEN
-        );
-    } catch (err) {
-        if (err && err.name == "TokenExpiredError")
-            return parseRefreshToken(req, res);
-        else return error;
-    }
-
-    if (
-        !decodedToken ||
-        (req.body.userId && req.body.userId !== decodedToken.userId)
-    )
-        return error;
-
-    return;
+const refreshAll = (res) => {
+    authenticationHandler.createToken();
+    res.cookie("refreshToken", authenticationHandler.createRefreshToken());
 };
+
+
 
 export default (req, res, next) => {
-    const error = parseToken(req, res);
-    if (error) res.status(error.statusCode).json({ error: error.message });
-    else next();
-};
 
+    const tokenParams = [
+        req.headers.authorization.split(" ")[1],
+        process.env.SECRET_TOKEN,
+    ];
+    const refreshTokenParams = [
+        req.cookies.refreshToken,
+        process.env.SECRET_REFRESH_TOKEN,
+    ];
+
+    const tokenIsValid = isValidToken(req, tokenParams);
+
+    if (tokenIsValid === false) return res.status(401).json({ message : 'Invalid token' });
+    else if (tokenIsValid === "expired") {
+        const refreshTokenIsValid = isValidToken(req, refreshTokenParams);
+
+        if (refreshTokenIsValid !== true) return res.status(401).json({ message : 'Invalid token' });
+        else refreshAll(res);
+    }
+
+    next();
+
+
+};
